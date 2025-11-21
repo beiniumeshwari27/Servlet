ba26b2a188d0a44d21890af0972bbc5f57f94d4a
beiniumeshwari27 <beiniumeshwari@gmail.com>
2025-11-21 15:12:15 +0530

    java programs

--- LoginServlet.java
+++ LoginServlet.java
@@ -1,38 +1,33 @@
 package com.codegnan.web;
 
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
  * Servlet implementation class LoginServlet
  */
-@WebServlet("/login")
+@WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.setContentType("text/html");
-		PrintWriter out=response.getWriter();
-		String name=request.getParameter("uname");
-		String password=request.getParameter("upwd");
-		out.println("<html><body>");
-		out.println("<h1 style='color:red'align='center'>");
-		if(name.equals("uma")&&password.equals("1234")) {
-			out.println("success");
-		
-		}else {
-			out.println("failure");
-		}
-		out.println("</h1>");
-		out.println("<h3 align='center'>");
-		out.println("<a href='./register.html'>|Login Form|</a>");
-		out.println("</h3><body></html>");
-		out.close();
+	response.setContentType("text/html");
+	String name=request.getParameter("uname");
+	String password=request.getParameter("upwd");
+	RequestDispatcher requestDispatcher=null;
+	if(name.equals("Naganjali")&&password.equals("beiniumeshwari@gmail.com")) {
+		requestDispatcher=request.getRequestDispatcher("/sucess.html");
+		requestDispatcher.forward(request, response);
+	}else {
+		requestDispatcher=request.getRequestDispatcher("/failure.html");
+		requestDispatcher.forward(request, response);
 	}
-
+	
+	}
 }
