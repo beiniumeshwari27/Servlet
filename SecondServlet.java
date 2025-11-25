6ec0daf193644b077f441a47fb94c4b530c003f3
beiniumeshwari27 <beiniumeshwari@gmail.com>
2025-11-25 16:49:45 +0530

    java programs

--- SecondServlet.java
+++ SecondServlet.java
@@ -1,40 +1,30 @@
 package com.codegnan.web;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
-import jakarta.servlet.ServletConfig;
-import jakarta.servlet.ServletContext;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebInitParam;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 /**
- * Servlet implementation class FirstServlet
+ * Servlet implementation class SecondServlet
  */
-@WebServlet(name="/SecondServlet",urlPatterns= {"/sec"},
-initParams= {@WebInitParam(name="e",value="EEE"),
-		@WebInitParam(name="f",value="FFF")
-})
+@WebServlet("/second")
 public class SecondServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	response.setContentType("text/html");
-	PrintWriter out=response.getWriter();
-	ServletConfig config=getServletConfig();
-	ServletContext context=getServletContext();
-	out.println("<html><body><h1>");
-	out.println("a---->"+context.getInitParameter("a")+"<br>");
-	out.println("b---->"+context.getInitParameter("b")+"<br>");
-	out.println("c---->"+config.getInitParameter("c")+"<br>");
-	out.println("d---->"+config.getInitParameter("d")+"<br>");
-	out.println("e---->"+config.getInitParameter("e")+"<br>");
-	out.println("f---->"+config.getInitParameter("f")+"<br>");
-	out.println("</h1></body></html>");
-	
+		float salary=Float.parseFloat(request.getParameter("esal"));
+		String address=request.getParameter("eaddr");
+		HttpSession hs=request.getSession();
+		hs.setAttribute("salary",salary);
+		hs.setAttribute("address",address);
+		RequestDispatcher rd=request.getRequestDispatcher("form3.html");
+		rd.forward(request, response);
+		
 	}
 
 }
